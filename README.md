da56c153dc0a4aac2f1147c9a0333226225cc72e
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2023-06-12 15:21:26 +0200

    Fallback message if rye run fails

--- README.md
+++ README.md
@@ -17,3 +17,20 @@ Project Status: Proof of Concept
 
 The name is currently Rye(ish) because it is not officially connected with Rye.
 
+## User Experience
+
+If the Rye(ish) kernel is used in a project where rye is not installed, or
+the rye project does not have an ipykernel, then starting the kernel fails.
+
+It starts a "fallback" kernel which that shows a message that rye is not setup
+as expected in this environment, and provide a regular ipython kernel which
+lets you run shell commands to fix rye!
+
+```diff
+- Failed to start Rye environment kernel - no ipykernel in rye project?
+- Run these:
+- !rye add ipykernel
+- !rye sync
+- 
+- Then restart the kernel to try again.
+```
--- src/ryeish_kernel/__main__.py
+++ src/ryeish_kernel/__main__.py
@@ -1,13 +1,17 @@
+import logging
 import platform
 import signal
 import subprocess
 import sys
 from pathlib import Path
 
+_logger = logging.getLogger(__name__)
+
 RUN_CMD = ["rye", "run"]
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     # Try to find pyproject.toml file
     # We do this so that we can spit out a better, more informative error
     # message if there is no pyproject.toml file present.
@@ -45,6 +49,7 @@ def main():
     exit_code = proc.wait()
     if exit_code != 0:
         print("ipykernel_launcher exited with error code:", exit_code, file=sys.stderr)
+        start_fallback_kernel()
 
 
 def find_pyproject_file():
@@ -55,6 +60,39 @@ def find_pyproject_file():
         if pyproject_file.exists():
             return pyproject_file
     return None
+
+
+def start_fallback_kernel():
+    """
+    Start a fallback kernel. Its purpose is
+
+    1. Show a message that rye is not setup as expected in this environment
+    2. Provide a regular ipython kernel which lets you run shell commands to fix rye!
+    """
+    help_messages = [
+        "Failed to start Rye environment kernel - no ipykernel in rye project?",
+        "Run these:",
+        "!rye add ipykernel",
+        "!rye sync",
+        "",
+        "Then restart the kernel to try again.",
+    ]
+
+    print("starting fallback kernel", file=sys.stderr)
+    for msg in help_messages:
+        print("ryeish-kernel:", msg, file=sys.stderr)
+
+    import ipykernel.kernelapp
+    import ipykernel.ipkernel
+    from ipykernel.kernelapp import IPKernelApp
+
+    class FallbackMessageKernel(ipykernel.ipkernel.IPythonKernel):
+        def do_execute(self, *args, **kwargs):
+            for msg in help_messages:
+                print(msg, file=sys.stderr)
+            return super().do_execute(*args, **kwargs)
+
+    IPKernelApp.launch_instance(kernel_class=FallbackMessageKernel)
 
 
 if __name__ == "__main__":
