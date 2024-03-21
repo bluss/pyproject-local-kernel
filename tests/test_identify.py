b40c160ce4619708c217dce898bb01ec8aaa898f
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-03-21 21:24:04 +0100

    Add tests with pytest

---
+++ tests/test_identify.py
@@ -0,0 +1,33 @@
+import pytest
+
+from pyproject_local_kernel import ProjectKind
+from pyproject_local_kernel import identify
+
+
+@pytest.mark.parametrize("path,expected", [
+    ("tests/identify/rye", ProjectKind.Rye),
+    ("tests/identify/poetry", ProjectKind.Poetry),
+    ("tests/identify/pdm", ProjectKind.Pdm),
+    ("tests/identify/hatch", ProjectKind.Hatch),
+    ("tests/identify/broken", ProjectKind.InvalidData),
+])
+def test_ident(path, expected):
+    pd = identify(path)
+    assert pd.kind == expected
+
+
+@pytest.mark.parametrize("path,cmd", [
+    ("tests/identify/custom", ["my", "cmd"])
+])
+def test_custom(path, cmd):
+    pd = identify(path)
+    assert pd.get_python_cmd() == cmd
+
+
+@pytest.mark.parametrize("path", [
+    "tests/identify/custom_broken",
+])
+def test_custom_error(path, caplog):
+    # Test log error
+    identify(path)
+    assert any('Is not a list or string' in rec.message for rec in caplog.records)
