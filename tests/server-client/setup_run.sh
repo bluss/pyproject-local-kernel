fcbd9f98c2ef347b9a594ad9212d455b00da1cde
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-03-24 19:17:59 +0100

    tests: Fix rye sync for windows
    Workaround a bug in Rye and uv.
    This looks like a Rye and uv interaction bug, only on Windows.
    
    We had the following requirement:
    
    pyproject-local-kernel @ file:///${PROJECT_ROOT}/../../..
    
    Wich worked in:
     - rye sync using uv on linux
     - uv install -r pyproject.toml on linux and windows
    
    However, on Windows rye sync using uv threw an error.
    
    Changing the requirement to this fixed version with extra indirection
    made the error go away on windows:
    
    pyproject-local-kernel @ file:///${PROJECT_ROOT}/../../../../pyproject-local-kernel
    
    The relevant rye version was 0.31+git-153213c and uv was 0.1.23.
    
    The error message was
    
    error: Requirement `pyproject-local-kernel @
    file:///${PROJECT_ROOT}/../../..` in
    `C:\Users\RUNNER~1\AppData\Local\Temp\.tmpETLCDR` looks like a directory
    but was passed as a package name. Did you mean `-e
    pyproject-local-kernel @ file:///${PROJECT_ROOT}/../../..`?

--- tests/server-client/setup_run.sh
+++ tests/server-client/setup_run.sh
@@ -10,17 +10,8 @@ RYE=${RYE:-rye}
 SPROJ=server/pyproject.toml
 CPROJ=client/pyproject.toml
 
-# use uv as a workaround for relative paths on windows
-
-if [ -n "$IS_WINDOWS" ]; then
-    UV=$HOME/.rye/uv/*/uv
-    (cd server; $UV venv; $UV pip install -r pyproject.toml)
-    (cd client; $UV venv; $UV pip install -r pyproject.toml)
-    sleep 1
-else
-    $RYE sync --pyproject $SPROJ
-    $RYE sync --pyproject $CPROJ
-fi
+$RYE sync --pyproject $SPROJ
+$RYE sync --pyproject $CPROJ
 $RYE run --pyproject $SPROJ jupytext --to ipynb client/notebook.py
 
 # Now execute the notebook
