f33379513a687efca801fa200d92c2b83c825868
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-03-24 16:57:10 +0100

    tests: Fix windows runner
    Can't get the relative paths in pyproject to work from rye (using uv) in
    windows, but it will work with uv directly.

--- tests/server-client/setup_run.sh
+++ tests/server-client/setup_run.sh
@@ -10,8 +10,16 @@ RYE=${RYE:-rye}
 SPROJ=server/pyproject.toml
 CPROJ=client/pyproject.toml
 
-$RYE sync --pyproject $SPROJ
-$RYE sync --pyproject $CPROJ
+# use uv as a workaround for relative paths on windows
+
+if [ -n "$IS_WINDOWS" ]; then
+    UV=$HOME/.rye/uv/*/uv
+    (cd server; $UV venv; $UV pip install -r pyproject.toml)
+    (cd client; $UV venv; $UV pip install -r pyproject.toml)
+else
+    $RYE sync --pyproject $SPROJ
+    $RYE sync --pyproject $CPROJ
+fi
 $RYE run --pyproject $SPROJ jupytext --to ipynb client/notebook.py
 
 # Now execute the notebook
