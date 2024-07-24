532800f941beb4bd9e2cb9efc3c834e933c51334
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-07-24 09:01:22 +0200

    Deduplicate notebook.py in tests

--- tests/server-client/setup_run.sh
+++ tests/server-client/setup_run.sh
@@ -12,11 +12,11 @@ CPROJ=client-rye/pyproject.toml
 
 $RYE sync --pyproject $SPROJ
 $RYE sync --pyproject $CPROJ
-$RYE run --pyproject $SPROJ jupytext --to ipynb client-rye/notebook.py
 (cd client-uv; uv sync)
 
 for nbdir in client-rye client-uv ; do
     # convert to ipynb
+    cp -v ./notebook.py $nbdir/
     $RYE run --pyproject $SPROJ jupytext --to ipynb $nbdir/notebook.py
     # execute the notebook
     $RYE run --pyproject $SPROJ papermill --cwd $nbdir $nbdir/notebook.ipynb $nbdir/notebook_out.ipynb
