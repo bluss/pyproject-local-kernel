510b0abca64abf8dfb3e834451550d8326ff256e
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-10-04 17:52:29 +0200

    tests: Python version fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,36 @@ if is_debugging():
         raise excinfo.value
 
 
+def pytest_addoption(parser):
+    parser.addoption("--use-python", type=str, default=None,
+                     help="Run integration tests vs these python major versions, space separated")
+
+
+class CurrentPython:
+    @pytest.fixture(scope="session")
+    def python_version(self):
+        "python major version to use"
+        return ".".join(str(elt) for elt in sys.version_info[:2])
+
+
+def python_versions_fixture_from_str(versions: str):
+    pyversions = versions.split()
+
+    class ConfiguredPython:
+        @pytest.fixture(scope="session", params=pyversions)
+        def python_version(self, request: pytest.FixtureRequest):
+            "python major version(s) to use"
+            return request.param
+
+    return ConfiguredPython()
+
+
 def pytest_configure(config: pytest.Config):
     config.addinivalue_line("markers", "server_args: extra uv sync arguments for server directory")
     config.addinivalue_line("markers", "unit: unit test")
     config.addinivalue_line("markers", "jupyter: jupyter integration test")
+
+    if config.option.use_python:
+        config.pluginmanager.register(python_versions_fixture_from_str(config.option.use_python))
+    else:
+        config.pluginmanager.register(CurrentPython())
