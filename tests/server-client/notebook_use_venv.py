95196d0d2e2984fe8b74403c103c582495c1702e
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2024-10-06 20:51:51 +0200

    tests: Add test for pyproject_local_kernel_use_venv
    Add papermill test for pyproject_local_kernel_use_venv

---
+++ tests/server-client/notebook_use_venv.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.6
+#   kernelspec:
+#     display_name: Pyproject Local
+#     language: python
+#     name: pyproject_local_kernel_use_venv
+# ---
+
+# %%
+import sys
+
+# %%
+sys.executable
+
+# %%
+sys.version_info
+
+# %%
+# try our local dependency
+import jinja2
