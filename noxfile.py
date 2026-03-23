efc1c7b483742d01fbcf065b28cefb7c1d9d1c3c
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2026-03-23 12:14:32 +0100

    Update to require Python 3.10

--- noxfile.py
+++ noxfile.py
@@ -14,12 +14,11 @@ nox.options.reuse_venv = "yes"
 nox.options.sessions = []
 
 python_versions = [
-    "3.8",
-    "3.9",
     "3.10",
     "3.11",
     "3.12",
     "3.13",
+    "3.14",
 ]
 python_short = ["py" + p.replace(".", "") for p in python_versions]
 windows_python_versions = [python_versions[1], python_versions[-1]]
